049c4c2b9ac90410bc5f184832f82c8293754edf
clf94-dev <65305851+clf94-dev@users.noreply.github.com>
2020-08-11 22:27:11 +0200

    added email validation with form hook
    Updated the styles file to sass framework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
+
 import React from "react";
 import { Grid, Button, TextField } from "@material-ui/core/";
-
+import Form from './Form';
 import Logo from "../images/logo.svg";
 import Illustration from "../images/illustration-dashboard.png";
+
+import '../styles/App.scss';
 
 function App() {
   return (
@@ -14,23 +17,7 @@ function App() {
           We are launching <strong>soon!</strong>
         </h1>
         <h4> Subscribe and get notified</h4>
-
-        <Grid container direction="row">
-          <Grid item md={9} xs={12}>
-            <TextField
-              id="outlined-basic"
-              label="Your email address..."
-              variant="outlined"
-              className="email-input"
-            />
-          </Grid>
-          <Grid item md={3} xs={12}>
-            <Button variant="contained" className="notify-btn">
-              {" "}
-              Notify Me
-            </Button>
-          </Grid>
-        </Grid>
+         <Form/>
         <img src={Illustration} alt="illustration" className="illust" />
       </Grid>
     </div>
---
+++ src/components/Form.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+import { Grid, Button, TextField } from "@material-ui/core/";
+import ErrorMessage from './ErrorMessage';
+
+function Form(){
+    const {handleSubmit, register, errors} = useForm();
+    const onSubmit = values => console.log(values);
+
+    return (
+
+        
+        <Grid container direction="row">
+          <Grid item md={9} xs={12}>
+           <form onSubmit={handleSubmit(onSubmit)}>
+               <input className="email-input" placeholder="Your email address..." name="email" ref={register({
+                   required:true, pattern:{
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                
+                  
+                   }
+               })}
+               />
+            
+               <ErrorMessage error={errors.email} />
+           </form>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            <Button variant="contained" className="notify-btn" type="submit" onClick={handleSubmit(onSubmit)}>
+              {" "}
+              Notify Me
+            </Button>
+          </Grid>
+        </Grid>
+    )
+}
+
+export default Form;
